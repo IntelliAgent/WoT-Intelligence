3072ffda523ab780d57380f6571875371d7190fb
Maxime Roussin-Bélanger <mroussin@hotmail.com>
2015-03-10 01:22:35 -0400

    Clean up

--- ReplayParser/src/main/java/ReplayParser.java
+++ ReplayParser/src/main/java/ReplayParser.java
@@ -1,42 +1,6 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class ReplayParser {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input replay path:");
-        String filename = scanner.nextLine();
-        File folder = new File(filename);
-        File[] listOfReplays = folder.listFiles();
 
-        assert listOfReplays != null;
-
-        for (File replay : listOfReplays) {
-            if (replay.isFile()) {
-               readReplay(replay);
-            }
-        }
-    }
-
-    private static void readReplay(File file) {
-        try {
-            String line = null;
-
-            // FileReader reads text files in the default encoding.
-            FileReader fileReader = new FileReader(file.getAbsoluteFile());
-
-            // Always wrap FileReader in BufferedReader.
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-            while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 }
