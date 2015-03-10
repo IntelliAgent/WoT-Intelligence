6509e1262971d398b7ae6febda2c45b4bd234d71
Maxime Roussin-Bélanger <mroussin@hotmail.com>
2015-03-10 00:29:51 -0400

    Can load the first json

--- ReplayDecoder/src/main/java/ca/unknown/replaydecoder/ReplayDecoder.java
+++ ReplayDecoder/src/main/java/ca/unknown/replaydecoder/ReplayDecoder.java
@@ -1,16 +1,7 @@
 package ca.unknown.replaydecoder;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-
-import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
-import java.io.*;
-import java.nio.ByteBuffer;
-import java.security.NoSuchAlgorithmException;
+import java.io.BufferedReader;
+import java.io.File;
 import java.util.Scanner;
 
 public class ReplayDecoder {
@@ -26,72 +17,16 @@ public class ReplayDecoder {
         BufferedReader encrypted = null;
         StringBuilder sb = new StringBuilder();
 
-        SecretKeySpec KS = new SecretKeySpec("SomeString".getBytes(), "Blowfish");
-        Cipher cipher = null;
-        try {
-            cipher = Cipher.getInstance("Blowfish");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        }
-
         for (File replay : listOfReplays) {
-            if (replay.isFile()) {
-                try {
-                    FileInputStream fis = new FileInputStream(replay);
-                    InputStreamReader isr = new InputStreamReader(fis, "UTF8");
-                    DataInputStream in = new DataInputStream(fis);
+            if (replay.isFile() && replay.getName().endsWith(".wotreplay")) {
+                ReplayFileReader replayFileReader = new ReplayFileReader(replay);
+                boolean goodMagicNumber = replayFileReader.validateMagicNumber();
+                int numberOfJSONBlocks = replayFileReader.getNumberOfJSONBlocks();
+                int firstBlockSize = replayFileReader.getFirstJSONBlockSize();
+                System.out.println(replayFileReader.getFirstJson(firstBlockSize));
 
-                    boolean goodMagicNumber = confirmMagicWoTNumber(in.readInt());
-                    int blocks = getNumberOfBlocks(in.readShort());
-                    System.out.println("Blocks : " + blocks);
-                    int firstBlockSize = getFirstBlockSize(in.readUnsignedShort());
-                    System.out.println("FirstBlockSize : " + firstBlockSize);
-                    int firstBlockSize2 = getFirstBlockSize(in.readUnsignedShort());
-                    int firstBlockSize3 = getFirstBlockSize(in.readUnsignedShort());
-                    BufferedReader br = new BufferedReader(isr);
-
-                    ByteBuffer allocate = ByteBuffer.allocate(firstBlockSize2);
-                    if (goodMagicNumber) {
-
-                        do {
-                            // /home/maxime/Desktop/Replays
-                            firstBlockSize2 += 255;
-                            while (firstBlockSize2 != 0) {
-                                firstBlockSize2--;
-                                byte c = in.readByte();
-                                sb.append((char) c);
-                            }
-                            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                            JsonParser jp = new JsonParser();
-                            JsonElement je = jp.parse(sb.toString());
-                            String prettyJsonString = gson.toJson(je);
-                            System.out.println(prettyJsonString);
-                            break;
-                        } while (in.read() != -1);
-                    }
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
         }
     }
 
-    private static int getFirstBlockSize(int i) {
-        System.out.println(i);
-        return i;
-    }
-
-    private static int getNumberOfBlocks(int i) {
-        System.out.println(i);
-        return i;
-    }
-
-    private static boolean confirmMagicWoTNumber(int i) {
-        String magic_number = "12323411";
-        return String.format("%04X", i).equals(magic_number);
-    }
 }
---
+++ ReplayDecoder/src/main/java/ca/unknown/replaydecoder/ReplayFileReader.java
@@ -0,0 +1,78 @@
+package ca.unknown.replaydecoder;
+
+import ca.unknown.replaydecoder.swapper.ByteSwapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+
+public class ReplayFileReader {
+
+    private static final String MAGIC_NUMBER = "12323411";
+    private static final int POS_NUMBER_JSON_BLOCKS = 4;
+    private static final int POS_SIZE_FIRST_JSON = 8;
+    private RandomAccessFile randomAccessFile;
+
+    public ReplayFileReader(File file) {
+        try {
+            randomAccessFile = new RandomAccessFile(file, "r");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean validateMagicNumber() {
+        try {
+            randomAccessFile.seek(0);
+            int magicNumberReadFromFile = randomAccessFile.readInt();
+            System.out.println(magicNumberReadFromFile);
+            return String.format("%04X", magicNumberReadFromFile).equals(MAGIC_NUMBER);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public int getNumberOfJSONBlocks() {
+        try {
+            randomAccessFile.seek(POS_NUMBER_JSON_BLOCKS);
+            return ByteSwapper.swap(randomAccessFile.readInt());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getFirstJSONBlockSize() {
+        try {
+            randomAccessFile.seek(POS_SIZE_FIRST_JSON);
+            return ByteSwapper.swap(randomAccessFile.readInt());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public String getFirstJson(int jsonSize) {
+        byte[] json = ByteBuffer.allocate(jsonSize).array();
+        try {
+            randomAccessFile.read(json, 0, jsonSize);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        StringBuilder buffer = new StringBuilder();
+        for (byte bit : json) {
+            buffer.append((char) bit);
+        }
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonParser jp = new JsonParser();
+        JsonElement je = jp.parse(buffer.toString());
+        return gson.toJson(je);
+    }
+}
