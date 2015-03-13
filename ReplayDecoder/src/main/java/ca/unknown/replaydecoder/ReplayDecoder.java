fc413aecd5ad7ca70281f6df0e09a7d83c56a1ec
Simon Gamache <simon.gamache-poirier.1@ulaval.ca>
2015-03-12 22:56:34 -0400

    Changed variable and method name to better reflect role and behavior

--- ReplayDecoder/src/main/java/ca/unknown/replaydecoder/ReplayDecoder.java
+++ ReplayDecoder/src/main/java/ca/unknown/replaydecoder/ReplayDecoder.java
@@ -28,10 +28,10 @@ public class ReplayDecoder {
                 if (!goodMagicNumber) {
                     break;
                 }
-                int numberOfJSONBlocks = replayFileReader.getNumberOfJSONBlocks();
+                int numberOfJSONBlocks = replayFileReader.getNumberOfBlocks();
                 System.out.println(numberOfJSONBlocks);
-                int firstBlockSize = replayFileReader.getFirstJSONBlockSize();
-                int secondBlockSize = replayFileReader.getSecondJSONBlockSize();
+                int firstBlockSize = replayFileReader.getFirstBlockSize();
+                int secondBlockSize = replayFileReader.getSecondBlockSize();
                 System.out.println("Second block size: " + secondBlockSize);
                 int cryptedSizePart = replayFileReader.getCryptedPartSize();
                 System.out.println("Crypted sized part : " + cryptedSizePart);
