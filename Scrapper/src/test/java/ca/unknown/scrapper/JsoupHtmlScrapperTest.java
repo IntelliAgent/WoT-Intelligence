9a8e65164e0b18378ccd08c8b52519d76984f1b5
Simon Gamache <simon.gamache-poirier.1@ulaval.ca>
2015-03-14 19:19:27 -0400

    New test scenarion with vbaddict

--- Scrapper/src/test/java/ca/unknown/scrapper/JsoupHtmlScrapperTest.java
+++ Scrapper/src/test/java/ca/unknown/scrapper/JsoupHtmlScrapperTest.java
@@ -7,27 +7,34 @@ import org.junit.Test;
 import ca.unknown.scrapper.action.Action;
 import ca.unknown.scrapper.action.FollowLinkAction;
 import ca.unknown.scrapper.action.ScrapeAction;
+import ca.unknown.scrapper.action.ExitAction;
 import ca.unknown.scrapper.invoker.ConcreteInvoker;
 import ca.unknown.scrapper.invoker.Invoker;
 import ca.unknown.scrapper.scrapeTarget.AttributeTarget;
 
 public class JsoupHtmlScrapperTest {
-
-	String entryPoint = "http://drmcninja.com/archives/comic/29p01/";
+	String entryPoint = "http://www.vbaddict.net/battlehistory.php?go=search";
+	
+	String replayPageLinkSelection = "#table-statistics tr td:last-child a";
 	
 	@Test
 	public void test() {
 		HtmlScrapper scrapper = new JsoupHtmlScrapper(entryPoint);
 		
-		Action scrape		= new ScrapeAction(scrapper, new AttributeTarget("#comic>img","title"));
-		Action followLink 	= new FollowLinkAction(scrapper, new AttributeTarget("a.next","href"));
-		
+		Action scrape		= new ScrapeAction(scrapper, new AttributeTarget(replayPageLinkSelection,"href"));
+		Action followLink 	= new FollowLinkAction(scrapper, new AttributeTarget("a[rel=next]","href"));
+		Action exit 		= new ExitAction(scrapper);
+				
 		scrape.setCallback(followLink);
 		followLink.setCallback(scrape);
+		followLink.setFailureCallback(exit);
 		
 		Invoker invoker 	= new ConcreteInvoker(scrape);
 		
-		invoker.start();
+		invoker.setMaxExecution(2); //Fetching for 5 pages
+		invoker.run();
+		
+		System.out.println("Number of scrap element " + scrapper.getScrapeResult().size());
 		
 		for(String scrapeResult : scrapper.getScrapeResult())
 			System.out.println(scrapeResult);
