ee02a88059798c9a966ec28ec1b97e7f736b4c54
Simon Gamache <simon.gamache-poirier.1@ulaval.ca>
2015-03-14 19:14:07 -0400

    Invoker now has more responsabilities regarding actions and has the power to stop execution

--- Scrapper/src/main/java/ca/unknown/scrapper/invoker/ConcreteInvoker.java
+++ Scrapper/src/main/java/ca/unknown/scrapper/invoker/ConcreteInvoker.java
@@ -1,10 +1,17 @@
 package ca.unknown.scrapper.invoker;
 
 import ca.unknown.scrapper.action.Action;
+import ca.unknown.scrapper.action.ExitAction;
 
 public class ConcreteInvoker implements Invoker{
 
-	Action firstAction;
+	private Action firstAction;
+	
+	private Action currentAction;
+	
+	private long maxExecution = Long.MAX_VALUE;
+	
+	private long execution = 0;
 	
 	public ConcreteInvoker(){
 		
@@ -15,13 +22,27 @@ public class ConcreteInvoker implements Invoker{
 	}
 
 	@Override
-	public void start() {
-		firstAction.execute();
+	public void setMaxExecution(long maxExecution) {
+		this.maxExecution = maxExecution;	
+	}
+	
+	@Override
+	public void run() {
+		currentAction = firstAction;
+		
+		while(!(currentAction instanceof ExitAction) && checkConditions()){
+			currentAction = currentAction.execute();
+			execution++;
+		}
 	}
 
 	@Override
 	public void setFirstAction(Action firstAction) {
 		this.firstAction = firstAction;
 	}
-
+	
+	private boolean checkConditions(){
+		return maxExecution == Long.MAX_VALUE ?
+				true : execution <= maxExecution;
+	}
 }
--- Scrapper/src/main/java/ca/unknown/scrapper/invoker/Invoker.java
+++ Scrapper/src/main/java/ca/unknown/scrapper/invoker/Invoker.java
@@ -3,7 +3,9 @@ package ca.unknown.scrapper.invoker;
 import ca.unknown.scrapper.action.Action;
 
 public interface Invoker {
-	public void start();
+	public void run();
 	
 	public void setFirstAction(Action firstAction);
+	
+	public void setMaxExecution(long maxExecution);
 }
