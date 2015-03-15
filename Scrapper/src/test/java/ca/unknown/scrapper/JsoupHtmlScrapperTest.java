b05ebe70f48a9674f84a6d4f9285a6f0e033170f
Simon Gamache <simon.gamache-poirier.1@ulaval.ca>
2015-03-15 00:44:07 -0400

    Test skeleton for asynchronous action

--- Scrapper/src/test/java/ca/unknown/scrapper/JsoupHtmlScrapperTest.java
+++ Scrapper/src/test/java/ca/unknown/scrapper/JsoupHtmlScrapperTest.java
@@ -8,13 +8,13 @@ import java.util.Collection;
 import org.junit.Test;
 
 import ca.unknown.scrapper.action.Action;
+import ca.unknown.scrapper.action.AsynchronousAction;
 import ca.unknown.scrapper.action.ExitAction;
 import ca.unknown.scrapper.action.FollowLinkAction;
 import ca.unknown.scrapper.action.ScrapeAction;
 import ca.unknown.scrapper.invoker.ConcreteInvoker;
 import ca.unknown.scrapper.invoker.Invoker;
 import ca.unknown.scrapper.scrapeTarget.AttributeTarget;
-import org.junit.Test;
 
 public class JsoupHtmlScrapperTest {
 	
@@ -48,6 +48,18 @@ public class JsoupHtmlScrapperTest {
 	
 	@Test
 	public void shouldStartThreeAsynchronousExecutionForScrapeResult(){
+		Thread t = new Thread();
 		
+		String entryPoint = "http://www.vbaddict.net/battlehistory.php?go=search";
+		
+		String replayPageLinkSelection = "#table-statistics tr td:last-child a";
+		
+		HtmlScrapper scrapper = new JsoupHtmlScrapper(entryPoint);
+		Collection<String> scraps = new ArrayList<String>();
+		
+		Action scrape		= new ScrapeAction(scrapper, new AttributeTarget(replayPageLinkSelection,"href"), scraps);
+
+		Action followLink 	= new FollowLinkAction(scrapper, new AttributeTarget("a[rel=next]","href"));
+		Action exit 		= new ExitAction(scrapper);
 	}
 }
