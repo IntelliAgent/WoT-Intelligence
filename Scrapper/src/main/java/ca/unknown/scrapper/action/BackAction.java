fe7464337b7a15941d9607f396d462d81a15598a
TheCoconutChef <thecoconutchef@gmail.com>
2015-05-17 12:01:50 -0400

    New BackAction

---
+++ Scrapper/src/main/java/ca/unknown/scrapper/action/BackAction.java
@@ -0,0 +1,12 @@
+package ca.unknown.scrapper.action;
+
+import ca.unknown.scrapper.HtmlScrapper;
+
+public class BackAction extends AbstractAction {
+
+  public BackAction(HtmlScrapper scraper) {
+    super(scraper);
+    // TODO Auto-generated constructor stub
+  }
+
+}
