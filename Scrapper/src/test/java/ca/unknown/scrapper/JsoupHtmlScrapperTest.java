17e60ef8d8d2f866def30b5b4548754c8f38e613
TheCoconutChef <thecoconutchef@gmail.com>
2015-05-17 16:13:39 -0400

    Non conforming test for post action

--- Scrapper/src/test/java/ca/unknown/scrapper/JsoupHtmlScrapperTest.java
+++ Scrapper/src/test/java/ca/unknown/scrapper/JsoupHtmlScrapperTest.java
@@ -4,68 +4,97 @@ import ca.unknown.scrapper.action.*;
 import ca.unknown.scrapper.invoker.ConcreteInvoker;
 import ca.unknown.scrapper.invoker.Invoker;
 import ca.unknown.scrapper.scrapeTarget.AttributeTarget;
+
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class JsoupHtmlScrapperTest {
-	
-	@Test
-	public void shouldPrintABunchOfRelativeLinkComingFromTheVbAddictBattleHistoryTable() {
-		String entryPoint = "http://www.vbaddict.net/battlehistory.php?go=search";
-		
-		String replayPageLinkSelection = "#table-statistics tr td:last-child a";
-		
-		HtmlScrapper scrapper = new JsoupHtmlScrapper(entryPoint);
-		Collection<String> scraps = new ArrayList<String>();
-		List<Map<String, List<String>>> redirects = new ArrayList<Map<String, List<String>>>();
-		
-		Action scrape		= new ScrapeAction(scrapper, new AttributeTarget(replayPageLinkSelection,"href"), scraps);
-		Action getHeader	= new GetRedirectResponseAction(scrapper, new AttributeTarget(replayPageLinkSelection, "href"), redirects);
-		Action followLink 	= new FollowLinkAction(scrapper, new AttributeTarget("a[rel=next]","href"));
-		Action exit 		= new ExitAction(scrapper);
-				
-		scrape.setCallback(getHeader);
-		getHeader.setCallback(followLink);
-		followLink.setCallback(scrape);
-		followLink.setFailureCallback(exit);
-		
-		Invoker invoker 	= new ConcreteInvoker(scrape);
-		
-		invoker.setMaxExecution(2); //Fetching for 2 pages
-		invoker.run();
-		
-		System.out.println("Number of scrap element " + scrapper.getScrapeResult().size());
-		
-		for(String scrapeResult : scraps)
-			System.out.println(scrapeResult);
-		
-		for(Map<String, List<String>> header : redirects){
-			for(Map.Entry<String, List<String>> entry : header.entrySet()){
-				System.out.print(entry.getKey() + " : ");
-				for(String values : entry.getValue())
-					System.out.println(values);
-			}
-		}
-	}
-	
-	@Test
-	public void shouldStartThreeAsynchronousExecutionForScrapeResult(){
-		Thread t = new Thread();
-		
-		String entryPoint = "http://www.vbaddict.net/battlehistory.php?go=search";
-		
-		String replayPageLinkSelection = "#table-statistics tr td:last-child a";
-		
-		HtmlScrapper scrapper = new JsoupHtmlScrapper(entryPoint);
-		Collection<String> scraps = new ArrayList<String>();
-		
-		Action scrape		= new ScrapeAction(scrapper, new AttributeTarget(replayPageLinkSelection,"href"), scraps);
 
-		Action followLink 	= new FollowLinkAction(scrapper, new AttributeTarget("a[rel=next]","href"));
-		Action exit 		= new ExitAction(scrapper);
-	}
+  @Test
+  public void shouldPrintABunchOfRelativeLinkComingFromTheVbAddictBattleHistoryTable() {
+    String entryPoint = "http://www.vbaddict.net/battlehistory.php?go=search";
+
+    String replayPageLinkSelection = "#table-statistics tr td:last-child a";
+
+    HtmlScrapper scrapper = new JsoupHtmlScrapper(entryPoint);
+    Collection<String> scraps = new ArrayList<String>();
+    List<Map<String, List<String>>> redirects = new ArrayList<Map<String, List<String>>>();
+
+    Action scrape = new ScrapeAction(scrapper, new AttributeTarget(replayPageLinkSelection, "href"), scraps);
+    Action getHeader = new GetRedirectResponseAction(scrapper, new AttributeTarget(replayPageLinkSelection, "href"), redirects);
+    Action followLink = new FollowLinkAction(scrapper, new AttributeTarget("a[rel=next]", "href"));
+    Action exit = new ExitAction(scrapper);
+
+    scrape.setCallback(getHeader);
+    getHeader.setCallback(followLink);
+    followLink.setCallback(scrape);
+    followLink.setFailureCallback(exit);
+
+    Invoker invoker = new ConcreteInvoker(scrape);
+
+    invoker.setMaxExecution(2); // Fetching for 2 pages
+    invoker.run();
+
+    System.out.println("Number of scrap element " + scrapper.getScrapeResult().size());
+
+    for (String scrapeResult : scraps)
+      System.out.println(scrapeResult);
+
+    for (Map<String, List<String>> header : redirects) {
+      for (Map.Entry<String, List<String>> entry : header.entrySet()) {
+        System.out.print(entry.getKey() + " : ");
+        for (String values : entry.getValue())
+          System.out.println(values);
+      }
+    }
+  }
+
+  @Test
+  public void onlyTakeBattleLinkThatHaveReplays() {
+    String entryPoint = "http://www.vbaddict.net/battlehistory.php?go=search";
+
+    String replayPageLinkSelection = "#table-statistics tr td:last-child a";
+
+    HtmlScrapper scrapper = new JsoupHtmlScrapper(entryPoint);
+    Collection<String> scraps = new ArrayList<String>();
+
+    Map<String, String> postParam = new HashMap<String, String>();
+
+    postParam.put("age", "0");
+    postParam.put("uploader", "");
+    postParam.put("playername", "");
+    postParam.put("server", "eu");
+    postParam.put("nation", "0");
+    postParam.put("tier", "0");
+    postParam.put("type", "0");
+    postParam.put("premium", "0");
+    postParam.put("mapid", "0");
+    postParam.put("modeid", "0");
+    postParam.put("replays", "1");
+
+    Action post = new PostRequestAction(scrapper, postParam, entryPoint);
+    Action scrape = new ScrapeAction(scrapper, new AttributeTarget(replayPageLinkSelection, "href"), scraps);
+    Action followLink = new FollowLinkAction(scrapper, new AttributeTarget("a[rel=next]", "href"));
+    Action exit = new ExitAction(scrapper);
+
+    post.setCallback(scrape);
+    post.setFailureCallback(exit);
+    scrape.setCallback(followLink);
+    followLink.setCallback(scrape);
+    followLink.setFailureCallback(exit);
+
+    Invoker invoker = new ConcreteInvoker(post);
+
+    invoker.setMaxExecution(2); // Fetching for 2 pages
+    invoker.run();
+
+    System.out.println("Number of scrap element for post " + scrapper.getScrapeResult().size());
+    for (String scrapeResult : scraps)
+      System.out.println(scrapeResult);
+  }
 }
