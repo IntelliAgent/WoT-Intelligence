9d074bd55229e321a13ca242e6f481bcfac50627
TheCoconutChef <thecoconutchef@gmail.com>
2015-05-23 14:47:06 -0400

    Removed unused exception

--- ReplayDecoder/src/main/java/ca/unknown/replaydecoder/exception/CannotValidateMagicNumber.java
+++
@@ -1,7 +0,0 @@
-package ca.unknown.replaydecoder.exception;
-
-public class CannotValidateMagicNumber extends RuntimeException {
-    public CannotValidateMagicNumber(String message, Exception e) {
-        super(message, e);
-    }
-}
