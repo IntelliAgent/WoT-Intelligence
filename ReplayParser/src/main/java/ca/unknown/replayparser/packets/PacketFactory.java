a914720db897a6b7a00a422f448aa9ab994fec94
TheCoconutChef <thecoconutchef@gmail.com>
2015-06-06 15:43:01 -0400

    Changed packet factory to use rawPacket

--- ReplayParser/src/main/java/ca/unknown/replayparser/packets/PacketFactory.java
+++ ReplayParser/src/main/java/ca/unknown/replayparser/packets/PacketFactory.java
@@ -18,60 +18,54 @@ public class PacketFactory {
     /**
      * The packet factory method delegates packet construction to the proper packet type
      */
-    public Packet createPacket(PacketType type, int length, float clock, ByteBuffer payload) {
-        switch (type) {
+    public Packet createPacket(RawPacket rawPacket) {
+        switch (rawPacket.getType()) {
             case BATTLE_LEVEL_SETUP:
-                return new Packet00(type, length, clock, payload);
+                return new Packet00(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
             case TANK_APPEARED:
-                return new Packet03(type, length, clock, payload);
+                return new Packet03(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
             case TANK_APPEARED_NEXT:
-                return new Packet05(type, length, clock, payload);
+                return new Packet05(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
             case VARIOUS_TANK_RELATED_UPDATES:
-                switch (getSubType(payload)) {
+                switch (rawPacket.getSubType()) {
                     case HEALTH_UPDATE:
-                        return new SubPacket0703(type, length, clock, payload);
+                        return new SubPacket0703(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
                     case DESTROYED_TRACK:
-                        return new SubPacket0707(type, length, clock, payload);
+                        return new SubPacket0707(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
                     default:
-                        return new Packet07(type, length, clock, payload);
+                        return new Packet07(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
                 }
             case VARIOUS_GAME_STATE_UPDATES:
-                switch (getSubType(payload)) {
+                switch (rawPacket.getSubType()) {
                     case UPDATE_VEHICLE:
-                        return new SubPacket080B(type, length, clock, payload);
+                        return new SubPacket080B(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
                     case UPDATE_VEHICLE_LIST:
-                        return new SubPacket0801(type, length, clock, payload);
+                        return new SubPacket0801(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
                     case UPDATE_VEHICLE_FRAG_COUNT:
-                        return new SubPacket0805(type, length, clock, payload);
+                        return new SubPacket0805(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
                     case VEHICLE_TRACKED:
-                        return new SubPacket0817(type, length, clock, payload);
+                        return new SubPacket0817(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
                     default:
-                        return new Packet08(type, length, clock, payload);
+                        return new Packet08(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
                 }
             case VEHICLE_POSITION_ROTATION_UPDATES:
-                return new Packet0a(type, length, clock, payload);
+                return new Packet0a(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
             case GAME_STATUS:
-                return new Packet12(type, length, clock, payload);
+                return new Packet12(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
             case FIRST_PACKET:
-                return new Packet14(type, length, clock, payload);
+                return new Packet14(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
             case MAIN_PLAYER_SPECIFIC_DATA:
-                return new Packet16(type, length, clock, payload);
+                return new Packet16(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
             case MAIN_PLAYER_ID:
-                return new Packet1e(type, length, clock, payload);
+                return new Packet1e(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
             case MAIN_PLAYER_SPECIFIC_DATA_NEXT:
-                return new Packet22(type, length, clock, payload);
+                return new Packet22(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
             case MAIN_PLAYER_SPECIFIC_DATA_NEXT_NEXT:
-                return new Packet26(type, length, clock, payload);
+                return new Packet26(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
             case LAST_PACKET:
-                return new PacketFF(type, length, clock, payload);
+                return new PacketFF(rawPacket.getType(), rawPacket.getPayloadLength(), rawPacket.getClock(), rawPacket.getPayload());
 
         }
         return null;
-    }
-
-    private SubPacketType getSubType(ByteBuffer payload) {
-        int subType = ByteSwapper.swap(payload.getInt(4));
-        payload.rewind();
-        return fromInt(subType);
     }
 }
